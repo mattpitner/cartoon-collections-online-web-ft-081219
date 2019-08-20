e8ee475eb03bbf26be2a42eb56d5118881962a10
Matt Pitner <mattpitner09@gmail.com>
2019-08-20 16:52:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,10 +27,9 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.detect do |cheese|
-        if cheese = nil 
-      return nil 
+        
     end 
-   
+    if cheese = "cheddar"
   end
- 
+ return nil
 end
